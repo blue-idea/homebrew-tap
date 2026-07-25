5e371be1af55a514fcd00bf8b54f8364a32ba746
blue-idea <manbudezhu@126.com>
2026-07-25 11:00:26 +0800

    style: order Homebrew Cask stanzas

--- Casks/linkit.rb
+++ Casks/linkit.rb
@@ -6,12 +6,13 @@ cask "linkit" do
   name "Linkit"
   desc "Smart knowledge curation space with AI-assisted organization and cloud sync"
   homepage "https://github.com/blue-idea/collection"
-  depends_on :macos
 
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  depends_on :macos
 
   app "Linkit.app"
 
